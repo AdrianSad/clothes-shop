b3f17beaf99536c7893faac38101f64eeea8ce02
skillexe10 <skillexe10@o2.pl>
2020-07-08 21:46:03 +0200

    pages initial commit and routing impl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage';
+import Default from './pages/Default';
+
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" exact component={About}/>
+      <Route path="/products" exact component={Products}/>
+      <Route path="/contact" exact component={Contact}/>
+      <Route path="/cart" component={Cart}/>
+      <Route path="/product/:id" component={SingleProduct}/>
+      <Route component={Default}/>
+    </Switch>
+    </>;
 }
 
 export default App;
